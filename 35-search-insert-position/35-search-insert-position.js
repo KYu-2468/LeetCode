db78ce1d9c5281d831940985a2cf6d666a3b2092
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-08-29 22:38:52 -0700

    Time: 104 ms (22.46%), Space: 42.3 MB (37.99%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    if(target < nums[0]) {
+        return 0;
+    }
+    
+    if(target > nums[nums.length - 1]) {
+        return nums.length;
+    }
+    
+    while(left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        
+        if(nums[mid] === target || (nums[mid] > target && nums[mid - 1] < target)) {
+            return mid;
+        } else if(nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+};
