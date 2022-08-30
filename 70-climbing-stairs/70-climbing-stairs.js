6f12c49286cbb1c1f2a2a92752737908ccb5c1c8
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-08-30 13:05:31 -0700

    Time: 65 ms (88.17%), Space: 41.9 MB (43.18%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const waysToClimb = [0, 1, 2];
+    
+    for(let i = 3; i <= n; i++) {
+        waysToClimb.push(waysToClimb[i - 1] + waysToClimb[i - 2]);
+    }
+    
+    return waysToClimb[n];
+};
