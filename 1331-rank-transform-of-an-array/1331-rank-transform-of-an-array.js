0ce1e0a086d9f66174f1180d3c26f09bd4ad3b1a
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-08-31 18:45:23 -0700

    Time: 840 ms (22.07%), Space: 97.5 MB (5.17%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    
+    const newArr = new Array(arr.length);
+    
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = [arr[i], i];
+    }
+    
+    arr.sort((a, b) => a[0] - b[0]);
+    
+    let rank = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        const [value, originalIndex] = arr[i];
+        let previousValue = -Infinity;
+        
+        if(i > 0) {
+            previousValue = arr[i - 1][0];
+        }
+        
+        if(value > previousValue) {
+            console.log(rank)
+            newArr[originalIndex] = ++rank;
+        } else {
+            newArr[originalIndex] = rank;
+        }
+    }
+    
+    return newArr;
+};
