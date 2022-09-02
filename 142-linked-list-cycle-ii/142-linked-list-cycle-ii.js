52862c2abe9078491beccd1533b8dc68d32e129e
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-02 11:45:27 -0700

    Time: 83 ms (91.40%), Space: 44.5 MB (91.89%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -12,18 +12,33 @@
  */
 var detectCycle = function(head) {
     let currentNode = head;
-    const map = new Map();
     
     while(currentNode) {
         
-        if(map.get(currentNode)) {
-            return map.get(currentNode);
+        if(currentNode.visited) {
+            return currentNode;
         } else {
-            map.set(currentNode, currentNode);
+            currentNode.visited = true;
         }
         
         currentNode = currentNode.next;
     }
     
     return null;
+    
+//     let currentNode = head;
+//     const map = new Map();
+    
+//     while(currentNode) {
+        
+//         if(map.get(currentNode)) {
+//             return map.get(currentNode);
+//         } else {
+//             map.set(currentNode, currentNode);
+//         }
+        
+//         currentNode = currentNode.next;
+//     }
+    
+//     return null;
 };
