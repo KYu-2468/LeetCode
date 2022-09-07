e47acafdac381c0edb0636f3177ccd50ea8ce66e
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-06 20:39:14 -0700

    Time: 112 ms (11.02%), Space: 42.2 MB (31.34%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -3,22 +3,33 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length === 1) {
-        return nums[0];
+    
+    if (nums.length === 0) return 0;
+    
+    const max = [0, nums[0]];
+    
+    for(let i = 1; i < nums.length; i++) {
+        max.push(Math.max(max[i - 1] + nums[i], max[i]));
     }
     
-    if(nums.length === 2) {
-        return Math.max(nums[0], nums[1]);
-    }
+    return max[max.length - 1]
     
-    const max = [nums[0], nums[1], nums[0] + nums[2]];
+//     if(nums.length === 1) {
+//         return nums[0];
+//     }
     
-    for(let i = 3; i < nums.length; i++) {
-        const comb1 = max[i - 2] + nums[i];
-        const comb2 = max[i - 3] + nums[i];
+//     if(nums.length === 2) {
+//         return Math.max(nums[0], nums[1]);
+//     }
+    
+//     const max = [nums[0], nums[1], nums[0] + nums[2]];
+    
+//     for(let i = 3; i < nums.length; i++) {
+//         const comb1 = max[i - 2] + nums[i];
+//         const comb2 = max[i - 3] + nums[i];
         
-        max.push(Math.max(comb1, comb2));
-    }
+//         max.push(Math.max(comb1, comb2));
+//     }
     
-    return Math.max(max[max.length - 1], max[max.length - 2]);
+//     return Math.max(max[max.length - 1], max[max.length - 2]);
 };
