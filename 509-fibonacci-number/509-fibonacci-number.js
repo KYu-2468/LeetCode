a3bc2eef4ea1704eb7905cd5968a8a0fccc6d19b
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-06 18:49:14 -0700

    Time: 99 ms (50.65%), Space: 42 MB (43.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,14 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    if(n === 0) {
-        return 0;
+var fib = function(n, map = {0: 0, 1: 1}) {
+    
+    if(map[n] !== undefined) {
+        return map[n];
     }
     
-    if(n === 1) {
-        return 1;
-    }
+    const result = fib(n-1, map) + fib(n - 2, map);
+    map[n] = result;
     
-    return fib(n-1) + fib(n - 2);
+    return result;
 };
