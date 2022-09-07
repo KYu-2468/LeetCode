954541089fdd832153bedda1e4f5ea1785ea7aee
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-06 19:46:27 -0700

    Time: 89 ms (43.86%), Space: 41.5 MB (93.50%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,11 +3,23 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    const arr = [0, 1, 2];
-    
-    for(let i = 3; i <= n; i++) {
-        arr.push(arr[i - 1] + arr[i - 2]);
+    if(n === 1) {
+        return 1;
     }
     
-    return arr[n];
+    const arr = [0, 1, 2];
+    let prev1 = 1;
+    let prev2 = 2;
+    
+    for(let i = 3; i <= n; i++) {
+        const p3 = prev1 + prev2;
+        
+        if(prev1 > prev2) {
+            prev2 = p3;
+        } else {
+            prev1 = p3;
+        }
+    }
+    
+    return Math.max(prev1, prev2);
 };
