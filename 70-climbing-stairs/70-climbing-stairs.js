8b05e7a5e6a6e6b9d582f939e28eed2450177eea
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-08 16:33:26 -0700

    Time: 73 ms (74.33%), Space: 41.5 MB (87.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,23 +3,12 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if(n === 1) {
-        return 1;
-    }
-    
-    const arr = [0, 1, 2];
-    let prev1 = 1;
-    let prev2 = 2;
+    let prev1 = 2;
+    let prev2 = 1;
     
     for(let i = 3; i <= n; i++) {
-        const p3 = prev1 + prev2;
-        
-        if(prev1 > prev2) {
-            prev2 = p3;
-        } else {
-            prev1 = p3;
-        }
+        [prev1, prev2] = [prev1 + prev2,prev1];
     }
     
-    return Math.max(prev1, prev2);
+    return n === 1? 1 : prev1;
 };
