deba0b10c4c71df1f3d05687793d77a65256e793
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-08 16:26:10 -0700

    Time: 78 ms (96.91%), Space: 48.8 MB (15.09%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.js
+++ 784-letter-case-permutation/784-letter-case-permutation.js
@@ -2,36 +2,20 @@
  * @param {string} s
  * @return {string[]}
  */
-var letterCasePermutation = function(s) {
-    const results = [];
-    const strArr = s.split("");
-    
-    const backtracking = (strArr = [], combo = []) => {
-                
-        if(strArr.length === 1 && Number.isNaN(+strArr[0])) {
-            
-            results.push([...combo, strArr[0].toUpperCase()].join(""));
-            results.push([...combo, strArr[0].toLowerCase()].join(""));
-            
-            return;
-        }
-        
-        if(strArr.length === 1) {
-            results.push([...combo, strArr[0]].join(""));
-            return;
-        }
-        
-        if(Number.isInteger(+strArr[0])) {
-            backtracking(strArr.slice(1), [...combo, strArr[0]]);
-            return;
-        }
-        
-        backtracking(strArr.slice(1), [...combo, strArr[0].toUpperCase()]);
-        backtracking(strArr.slice(1), [...combo, strArr[0].toLowerCase()]);
-        
+var letterCasePermutation = function(s, start = 0, results = [], comb = []) {
+    if(comb.length === s.length) {
+        results.push(comb.join(""));
+        return;
     }
     
-    backtracking(strArr);
+        
+    if(!Number.isInteger(+s.charAt(start))) {
+        letterCasePermutation(s, start + 1, results, [...comb, s.charAt(start).toUpperCase()]);
+        letterCasePermutation(s, start + 1, results, [...comb, s.charAt(start).toLowerCase()]);
+    } else {
+        comb.push(s.charAt(start));
+        letterCasePermutation(s, start + 1, results, comb);
+    }
     
     return results;
 };
