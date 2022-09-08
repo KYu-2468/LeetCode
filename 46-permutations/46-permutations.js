ce432ffb94772ce9aea4da8b82dfe82a9106822a
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-08 15:29:14 -0700

    Time: 140 ms (12.67%), Space: 45.1 MB (43.16%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -2,17 +2,16 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums, results  = [], combos = []) {
-    
-    if(nums.length === 1) {
-        results.push([...combos, nums[0]]);
-        return results;
+var permute = function(nums, results = [], comb = []) {
+    if(comb.length === nums.length) {
+        results.push(comb);
+        return;
     }
     
     for(let i = 0; i < nums.length; i++) {
-        
-        permute([...nums.slice(0, i), ...nums.slice(i + 1)], results, [...combos, nums[i]]);
-        
+        if(!comb.includes(nums[i])) {
+            permute(nums, results, [...comb, nums[i]]);
+        }
     }
     
     return results;
