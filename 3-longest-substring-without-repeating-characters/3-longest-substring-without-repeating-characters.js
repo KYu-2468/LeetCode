fa43d4dc9cb41bf0ec8fbb927e09d054d5bef983
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-07 22:35:11 -0700

    Time: 133 ms (65.73%), Space: 49.2 MB (23.20%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max = 0;
+    let start = 0;
+    const map = {};
+    
+    for(let i = 0; i < s.length; i++) {
+        if(map[s[i]] === undefined || map[s[i]] < start) {
+            map[s[i]] = i;
+            max = Math.max(max, i - start + 1);
+        } else {
+            start = map[s[i]] + 1;
+            map[s[i]] = i;
+        }
+    }
+    
+    return max;
+};
