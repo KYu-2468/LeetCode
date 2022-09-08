2da8d1c7e0ffe53d178ae06d1088e23e8e4ba773
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-08 13:23:29 -0700

    Time: 118 ms (52.12%), Space: 44.7 MB (75.38%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -3,65 +3,43 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    const queue = [];
-    addMin(grid, queue);
-    const dir = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    let fresh = 0;
+    let rotten = [];
     
-    while(queue.length) {
-        const [row, col] = queue[0];
-        
-        dir.forEach(([r, c]) => {
-            
-            if(row + r >= 0 && row + r < grid.length && col + c >= 0 && col + c < grid[0].length) {
-                const neighbor = grid[row + r][col + c];
-                
-                if(neighbor.val === 1) {
-                    neighbor.val = 2;
-                    neighbor.min = grid[row][col].min + 1;
-                    queue.push([row + r, col + c]);
-                } else if (neighbor.val === 2) {
-                    neighbor.min = Math.min(neighbor.min, grid[row][col].min + 1);
-                }
-                
+    for(let r = 0; r < grid.length; r++) {
+        for(let c = 0; c < grid[0].length; c++) {
+            if(grid[r][c] === 1) {
+                fresh++;
+            } else if(grid[r][c] === 2) {
+                rotten.push([r, c]);
             }
-        })
-        
-        queue.shift();
+        }
     }
     
-    return findMaxMinute(grid);
+    const infect = (r, c, infected) => {
+        if(r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] === 1) {
+            grid[r][c] = 2;
+            infected.push([r, c]);
+            fresh--;
+        }
+    }
+    
+    let sec = 0;
+    
+    while(fresh && rotten.length) {
+        const infected = [];
+        
+        for(let i = 0; i < rotten.length; i++) {
+            const [r, c] = rotten[i];
+            
+            infect(r + 1, c, infected);
+            infect(r - 1, c, infected);
+            infect(r, c + 1, infected);
+            infect(r, c - 1, infected);
+        }
+        sec++;
+        rotten = infected;
+    }
+    
+    return fresh ? -1 : sec;
 };
-
-const findMaxMinute = (grid) => {
-    let maxMinute = 0;
-    
-    for(let i = 0; i < grid.length; i++) {
-        
-        for(let j = 0; j < grid[0].length; j++) {
-            
-            if(grid[i][j].val === 1) {
-                return -1;
-            }
-            
-            maxMinute = Math.max(grid[i][j].min, maxMinute);
-        }
-    }
-    
-    return maxMinute;
-}
-
-
-const addMin = (grid, queue) => {
-    
-    for(let i = 0; i < grid.length; i++) {
-        
-        for(let j = 0; j < grid[0].length; j++) {
-            if(grid[i][j] === 2) {
-                queue.push([i, j]);
-            }
-            
-            const item = {val: grid[i][j], min: 0};
-            grid[i][j] = item;
-        }
-    }
-}
