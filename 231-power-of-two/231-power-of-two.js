5815938deb5a46a55e9edd1aa8ec9764054127fc
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-08 21:43:37 -0700

    Time: 76 ms (92.89%), Space: 42.6 MB (94.23%) - LeetHub

--- 231-power-of-two/231-power-of-two.js
+++ 231-power-of-two/231-power-of-two.js
@@ -3,15 +3,22 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    let i = 0;
-    let total = 1;
-    while(total <= n) {
-        total = 2 ** i;
-        if(total === n) {
-            return true;
-        }
-        
-        i++;
+    
+    if(n <= 0) {
+        return false;
     }
-    return false;
+    
+    return (n & (n - 1)) === 0;
+    
+//     let i = 0;
+//     let total = 1;
+//     while(total <= n) {
+//         total = 2 ** i;
+//         if(total === n) {
+//             return true;
+//         }
+        
+//         i++;
+//     }
+//     return false;
 };
