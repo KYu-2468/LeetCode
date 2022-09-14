275ca79dc56e6d92213703c496e7cba4be7e86b2
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-13 22:04:33 -0700

    Time: 98 ms (43.88%), Space: 42.6 MB (33.42%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,21 +3,33 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const arr = new Array(prices.length).fill(0);
     
-    for(let i = prices.length - 2; i >= 0; i--) {
-        for(let j = i + 1; j < prices.length; j++) {
-            if(prices[i] < prices[j]) {
-                let m = 0;
-                for(let n = j + 1; n < prices.length; n++) {
-                    m = Math.max(m, arr[n]);
-                }
-                
-                arr[i] = Math.max(arr[i], prices[j] - prices[i] + m);
-            }
+    let profit = 0;
+    
+    for(let i = 0; i < prices.length - 1; i++) {
+        if(prices[i + 1] > prices[i]) {
+            profit += prices[i + 1] - prices[i];
         }
-        
     }
     
-    return Math.max(...arr)
+    return profit;
+    
+    
+//     const arr = new Array(prices.length).fill(0);
+    
+//     for(let i = prices.length - 2; i >= 0; i--) {
+//         for(let j = i + 1; j < prices.length; j++) {
+//             if(prices[i] < prices[j]) {
+//                 let m = 0;
+//                 for(let n = j + 1; n < prices.length; n++) {
+//                     m = Math.max(m, arr[n]);
+//                 }
+                
+//                 arr[i] = Math.max(arr[i], prices[j] - prices[i] + m);
+//             }
+//         }
+        
+//     }
+    
+//     return Math.max(...arr);
 };
