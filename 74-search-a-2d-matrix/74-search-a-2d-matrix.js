c66e96db1bf03a550b8a192a2f25934e85c2fb40
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-13 23:29:37 -0700

    Time: 102 ms (28.78%), Space: 43.9 MB (6.72%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,10 +4,41 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    for(let r = 0; r < matrix.length; r++) {
-        if(matrix[r].includes(target)) {
-            return true;
+    
+    if(target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) return false;
+    
+    let left = 0;
+    let right = matrix.length - 1;
+    let row = 0;
+    
+    while(left <= right) {
+        const mid = Math.floor((right + left) / 2);
+        
+        if(matrix[mid][0] <= target && (mid === matrix.length - 1 || matrix[mid + 1][0] > target)) {
+            row = mid;
+            break;
+        } else if(matrix[mid][0] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+    
+    left = 0;
+    right = matrix[0].length - 1;
+    
+    while(left <= right) {
+        const mid = Math.floor((right + left) / 2);
+        console.log(mid)
+        if(matrix[row][mid] === target) {
+            return true;
+        } else if(matrix[row][mid] < target) {
+            console.log('hit', mid, right)
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
     return false;
 };
