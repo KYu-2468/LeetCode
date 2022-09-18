a339fca9a79e5b3d7f5986d82e10643abc247063
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-17 22:48:37 -0700

    Time: 121 ms (14.15%), Space: 43.8 MB (24.53%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialArray = function(nums) {
+    nums.sort((a, b) => a - b);
+    
+    if(nums.length <= nums[0]) {
+        return nums.length;
+    }
+    
+    let left = 0;
+    let right = nums.length;
+    
+    while(left <= right) {
+        const mid = Math.floor((right + left) / 2);
+        const numOfLarger = nums.length - mid;
+        console.log(numOfLarger)
+        if((numOfLarger === nums[mid] && numOfLarger > nums[mid - 1]) || (numOfLarger > nums[mid - 1] && numOfLarger < nums[mid])) {
+            return numOfLarger;
+        } else if(numOfLarger > nums[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+};
