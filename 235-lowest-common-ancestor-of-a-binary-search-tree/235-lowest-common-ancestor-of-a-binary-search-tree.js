cd51f6176613671a9e95565e279f512d16403cf7
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-18 23:11:30 -0700

    Time: 99 ms (82.74%), Space: 52.2 MB (62.52%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,13 +13,29 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if(root.val === p.val || root.val === q.val) {
-        return root;
-    } else if (p.val > root.val && q.val > root.val) {
-        return lowestCommonAncestor(root.right, p, q);
-    } else if (p.val < root.val && q.val < root.val) {
-        return lowestCommonAncestor(root.left, p, q);
-    } else {
-        return root;
+    
+    let curNode = root;
+    
+    while(curNode) {
+        if(curNode.val === p.val || curNode.val === q.val) {
+            return curNode;
+        } else if (p.val > curNode.val && q.val > curNode.val) {
+            curNode = curNode.right;
+        } else if (p.val < curNode.val && q.val < curNode.val) {
+            curNode = curNode.left;
+        } else {
+            return curNode;
+        }
     }
+    
+    
+    // if(root.val === p.val || root.val === q.val) {
+    //     return root;
+    // } else if (p.val > root.val && q.val > root.val) {
+    //     return lowestCommonAncestor(root.right, p, q);
+    // } else if (p.val < root.val && q.val < root.val) {
+    //     return lowestCommonAncestor(root.left, p, q);
+    // } else {
+    //     return root;
+    // }
 };
