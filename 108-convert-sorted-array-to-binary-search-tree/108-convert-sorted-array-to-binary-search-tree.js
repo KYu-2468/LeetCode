4f4b2afea868bb9a8d72a6c0365bfdc4c86b85aa
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-18 23:53:49 -0700

    Time: 73 ms (96.94%), Space: 44.8 MB (20.57%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -10,14 +10,60 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function(nums) {
-    if(nums.length === 0) return null;
+var sortedArrayToBST = function(nums, start = 0, end = nums.length - 1) {
     
-    const mid = Math.floor(nums.length / 2);
+    if(start > end) return null;
+    
+    const mid = Math.floor((start + end) / 2);
     const root = new TreeNode(nums[mid]);
     
-    root.left = sortedArrayToBST(nums.slice(0, mid));
-    root.right = sortedArrayToBST(nums.slice(mid + 1));
+    if(start === end) return root;
+    
+    root.left = sortedArrayToBST(nums, start, mid - 1);
+    root.right = sortedArrayToBST(nums, mid + 1, end);
+    
+    return root;
+    
+//     if(nums.length === 0) return null;
+    
+//     const mid = Math.floor(nums.length / 2);
+//     const root = new TreeNode(nums[mid]);
+    
+//     root.left = sortedArrayToBST(nums.slice(0, mid));
+//     root.right = sortedArrayToBST(nums.slice(mid + 1));
+    
+//     return root;
+};
+
+const recurWithSlice = (nums) => {
+    const start = performance.now();
+    sortedArrayToBST(nums);
+    console.log(performance.now() - start);
+}
+
+var sortedArrayToBST_1 = function(nums, start, end) {
+    const mid = Math.floor((start + end) / 2);
+    const root = new TreeNode(nums[mid]);
+    
+    if(start === end) return root;
+    
+    root.left = sortedArrayToBST(nums, start, mid);
+    root.right = sortedArrayToBST(nums, mid + 1, end);
     
     return root;
 };
+
+const recurWithPointer = (nums) => {
+    const start = performance.now();
+    sortedArrayToBST_1(nums);
+    console.log(performance.now() - start);
+}
+
+
+// class TreeNode {
+//     constructor(val, left, right) {
+//         this.val = (val===undefined ? 0 : val);
+//         this.left = (left===undefined ? null : left);
+//         this.right = (right===undefined ? null : right);
+//     }
+// }
