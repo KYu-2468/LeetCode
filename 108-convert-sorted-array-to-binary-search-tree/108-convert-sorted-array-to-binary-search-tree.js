6d0c86a3577bb9df7376e174a2cb689c11854e35
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-19 00:01:24 -0700

    Time: 119 ms (39.56%), Space: 44.5 MB (63.28%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -10,8 +10,35 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
+
+// class TreeNode {
+//     constructor(val, left, right) {
+//         this.val = (val===undefined ? 0 : val);
+//         this.left = (left===undefined ? null : left);
+//         this.right = (right===undefined ? null : right);
+//     }
+// }
+
 var sortedArrayToBST = function(nums, start = 0, end = nums.length - 1) {
     
+    if(nums.length === 0) return null;
+    
+    const mid = Math.floor(nums.length / 2);
+    const root = new TreeNode(nums[mid]);
+    
+    root.left = sortedArrayToBST(nums.slice(0, mid));
+    root.right = sortedArrayToBST(nums.slice(mid + 1));
+    
+    return root;
+};
+
+const recurWithSlice = (nums) => {
+    const start = performance.now();
+    sortedArrayToBST(nums);
+    console.log(performance.now() - start);
+}
+
+var sortedArrayToBST_1 = function(nums, start, end) {
     if(start > end) return null;
     
     const mid = Math.floor((start + end) / 2);
@@ -23,34 +50,6 @@ var sortedArrayToBST = function(nums, start = 0, end = nums.length - 1) {
     root.right = sortedArrayToBST(nums, mid + 1, end);
     
     return root;
-    
-//     if(nums.length === 0) return null;
-    
-//     const mid = Math.floor(nums.length / 2);
-//     const root = new TreeNode(nums[mid]);
-    
-//     root.left = sortedArrayToBST(nums.slice(0, mid));
-//     root.right = sortedArrayToBST(nums.slice(mid + 1));
-    
-//     return root;
-};
-
-const recurWithSlice = (nums) => {
-    const start = performance.now();
-    sortedArrayToBST(nums);
-    console.log(performance.now() - start);
-}
-
-var sortedArrayToBST_1 = function(nums, start, end) {
-    const mid = Math.floor((start + end) / 2);
-    const root = new TreeNode(nums[mid]);
-    
-    if(start === end) return root;
-    
-    root.left = sortedArrayToBST(nums, start, mid);
-    root.right = sortedArrayToBST(nums, mid + 1, end);
-    
-    return root;
 };
 
 const recurWithPointer = (nums) => {
@@ -59,11 +58,11 @@ const recurWithPointer = (nums) => {
     console.log(performance.now() - start);
 }
 
-
-// class TreeNode {
-//     constructor(val, left, right) {
-//         this.val = (val===undefined ? 0 : val);
-//         this.left = (left===undefined ? null : left);
-//         this.right = (right===undefined ? null : right);
-//     }
-// }
+const generateArr = (num) => {
+    const arr = [];
+    for(let i = -num; i <= num; i++) {
+        arr.push(i);
+    }
+    
+    return arr;
+}
