cc990a7919ada530d36a13630916ecfd004726fe
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-27 15:36:33 -0700

    Time: 219 ms (32.83%), Space: 51 MB (29.57%) - LeetHub

--- 77-combinations/77-combinations.js
+++ 77-combinations/77-combinations.js
@@ -9,13 +9,11 @@ const combine = (n, k) => {
     const backtrack = (arr, index) => {
         // base case
         if (arr.length === k) {
-            results.push([...arr]);
+            results.push(arr);
             return;
         }
         for(let i = index; i <= n; i++) {
-            arr.push(i);
-            backtrack(arr, i + 1);
-            arr.pop();
+            backtrack([...arr, i], i + 1);
         }
     }
 
