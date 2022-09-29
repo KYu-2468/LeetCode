5572fb270a660c7f97075e675b1623a51c2e5f75
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-28 18:08:57 -0700

    Time: 141 ms (63.05%), Space: 45.8 MB (21.72%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -5,24 +5,8 @@
 var largestPerimeter = function(nums) {
     nums.sort((a, b) => a - b);
     
-    const isValidTri = (s1, s2, s3) => {
-        if(s1 + s2 <= s3) {
-            return false;
-        }
-        
-        if(s1 + s3 <= s2) {
-            return false;
-        }
-        
-        if(s2 + s3 <= s1) {
-            return false;
-        }
-        
-        return true;
-    }
-    
     for(let i = nums.length - 1; i >= 2; i--) {
-        if(isValidTri(nums[i], nums[i - 1], nums[i - 2])) {
+        if(nums[i - 1] + nums[i - 2] > nums[i]) {
             return nums[i] + nums[i - 1] + nums[i - 2];
         }
     }
