bc6ec62667957a0e9b866391bc908284901d2eef
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-09-30 10:46:52 -0700

    Time: 169 ms (57.08%), Space: 49.3 MB (50.47%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -10,12 +10,13 @@ var merge = function(intervals) {
     
     for(let i = 1; i < intervals.length; i++) {
         const lastInterval = result[result.length - 1];
+        
         if(lastInterval[1] >= intervals[i][0]) {
             lastInterval[1] = Math.max(lastInterval[1], intervals[i][1]);
-        } else {
-            result.push(intervals[i]);
+            continue;
         }
         
+        result.push(intervals[i]);
     }
     
     return result;
