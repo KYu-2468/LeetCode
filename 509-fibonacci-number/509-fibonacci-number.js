dc5d5bf09eea904f2363f5152489394af9a9d248
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-10-03 22:32:00 -0700

    Time: 76 ms (85.02%), Space: 42.1 MB (20.86%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,14 +2,15 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n, map = {0: 0, 1: 1}) {
+var fib = function(n) {
+    let prev1 = 1;
+    let prev2 = 0;
     
-    if(map[n] !== undefined) {
-        return map[n];
+    if(n <= 1) return n;
+    
+    for(let i = 2; i <= n; i++) {
+        [prev1, prev2] = [prev1 + prev2, prev1];
     }
     
-    const result = fib(n-1, map) + fib(n - 2, map);
-    map[n] = result;
-    
-    return result;
+    return prev1;
 };
