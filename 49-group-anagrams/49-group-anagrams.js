5b17a61d481885bd1ad3fc1f0c1027a0d02fc0f3
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-10-04 15:18:28 -0700

    Time: 200 ms (64.47%), Space: 54 MB (37.28%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,6 +3,18 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    
+    let groups = {}
+    strs.forEach(str => {
+        const sortedStr = str.split('').sort().join('')
+        if (groups[sortedStr]) {
+            groups[sortedStr].push(str)
+        } else {
+            groups[sortedStr] = [str]
+        }
+    })
+    return Object.values(groups)
+    
     const root = {};
     const set = new Set();
     
