e453c1906df2b6288489f75c08a7f11126440e29
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-10-10 23:24:05 -0700

    Time: 112 ms (80.01%), Space: 48.7 MB (31.88%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    const result = [];
+    candidates.sort((a, b) => a - b);
+    
+    const backtrack = (index = 0, total = 0, comb = []) => {
+        if(total > target) return;
+        
+        if(total === target) {
+            return result.push(comb);
+        }
+        
+        for(let i = index; i < candidates.length; i++) {
+            backtrack(i, total + candidates[i], [...comb, candidates[i]]);
+        }
+        
+    }
+    
+    backtrack();
+    
+    return result;
+};
