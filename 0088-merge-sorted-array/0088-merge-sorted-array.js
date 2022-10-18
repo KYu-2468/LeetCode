e4cabc8c3e799adade5f02fcdb63fd3fa4fafbac
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-10-18 09:16:28 -0700

    Time: 60 ms (97.23%), Space: 41.9 MB (73.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,30 +6,12 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    const outputArr = new Array(m + n);
-    let p1 = 0;
-    let p2 = 0;
+    let index = m + n - 1;
+    m--;
+    n--;
     
-    for(let i = 0; i < m + n; i++) {
-        if (p1 >= m) {
-            outputArr[i] = nums2[p2];
-            p2++;
-        } else if (p2 >= n) {
-            outputArr[i] = nums1[p1];
-            p1++;
-        } else {
-            if (nums1[p1] <= nums2[p2]) {
-                outputArr[i] = nums1[p1];
-                p1++;
-            } else {
-                outputArr[i] = nums2[p2];
-                p2++;
-            }
-        }
-    }
-    
-    
-    for(let num in outputArr) {
-        nums1[num] = outputArr[num];
+    while(n >= 0) {
+        nums1[index] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        index--;
     }
 };
