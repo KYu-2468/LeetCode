bb950a2d9c57002ea6e5ea2d04df3c7d9d495d0d
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-10-25 00:33:03 -0700

    Time: 92 ms (67.30%), Space: 42.3 MB (46.96%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let max = 0;
+    let count = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === '(') {
+            count++;
+            max = Math.max(max, count);
+        }
+        
+        if(s[i] === ")") {
+            count--;
+        }
+    }
+    
+    return max;
+};
