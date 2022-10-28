43d3451d5e837eb8370cb519aa3ddd065fd8bdcb
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-10-28 15:46:18 -0700

    Time: 171 ms (7.83%), Space: 49.6 MB (5.26%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    const leftMax = new Array(height.length).fill(0);
+    const rightMax = new Array(height.length).fill(0);
+    let sum = 0;
+    
+    for(let i = 1; i < leftMax.length; i++) {
+        leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
+    }
+    
+    for(let i = rightMax.length - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(rightMax[i + 1], height[i + 1]);
+    }
+    console.log(leftMax);
+    console.log(rightMax);
+    
+    for(let i = 1; i < height.length - 1; i++) {
+        sum += Math.max(0, Math.min(leftMax[i], rightMax[i]) - height[i]);
+    }
+    
+    return sum;
+};
