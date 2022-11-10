275a47740ff0078e52bce0cc177dbf32477e2766
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-10 00:07:18 -0800

    Time: 519 ms (58.84%), Space: 60.2 MB (45.43%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.js
+++ 0901-online-stock-span/0901-online-stock-span.js
@@ -1,6 +1,7 @@
 
 var StockSpanner = function() {
     this.array = [];
+    this.max = [];
 };
 
 /** 
@@ -8,13 +9,41 @@ var StockSpanner = function() {
  * @return {number}
  */
 StockSpanner.prototype.next = function(price) {
-    let count = 1;
-    
-    for(let i = this.array.length - 1; i >= 0; i--) {
-        if(this.array[i] <= price) count++;
-        else break;
+    // console.log('pre: ', this.array, this.max, price)
+    if(this.array.length === 0 || price < this.array[this.array.length - 1]) {
+        this.array.push(price);
+        this.max.push(this.array.length - 1);
+        return 1;
     }
+    
+    if(price >= this.array[this.max[0]]) {
+        this.array.push(price);
+        this.max = [this.array.length - 1];
+        return this.array.length;
+    }
+    
+    let left = 0, right = this.max.length - 1, mid = 0;
+    while(left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if(left === right) {
+            break;
+        }
+        else if(price >= this.array[this.max[mid]]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    // console.log('mid: ', mid)
     this.array.push(price);
+    for(let i = this.max.length - 1; i >= mid; i--) {
+        this.max.pop();
+    }
+    // console.log('cur: ', this.array, this.max, mid, price)
+    this.max.push(this.array.length - 1);
+    
+    // console.log('post: ', this.array, this.max, mid, price)
+    const count = this.array.length - ((this.max[mid - 1] || 0) + 1);
     return count;
 };
 
