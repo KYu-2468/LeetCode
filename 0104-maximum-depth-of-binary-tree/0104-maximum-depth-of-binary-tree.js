416194cb4f930161a099c1431509b5794e6713be
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-16 09:10:57 -0800

    Time: 86 ms (82.03%), Space: 44.7 MB (89.68%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    let max = 0;
+    
+    const searchDepth = (node, depth) => {
+        max = Math.max(max, depth);
+        if (node.left) {
+            searchDepth(node.left, depth + 1);
+        }
+        
+        if (node.right) {
+            searchDepth(node.right, depth + 1);
+        }
+    }
+    searchDepth(root, 1)
+    return max;
+};
