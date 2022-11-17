10bf1871381b9d54c4782b771be829d6298d8600
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-17 09:05:36 -0800

    Time: 83 ms (83.24%), Space: 43.7 MB (91.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,40 +11,63 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
-    if(!list1) {
-        return list2;
+    if (!list1 && !list2) {return null}
+
+  let list = new ListNode()
+  let head = list
+
+  while (list1 && list2){
+    if (list1.val <= list2.val){
+      head.next = list1
+      head = list1
+      list1 = list1.next
     }
-    
-    if(!list2) {
-        return list1;
+    else {
+      head.next = list2
+      head = list2
+      list2 = list2.next
     }
+  }
+
+  if (!list1) {head.next = list2}
+  if (!list2) {head.next = list1}
+
+  return list.next
     
-    let l3 = {};
-    let l1 = list1;
-    let l2 = list2;
     
-    while(l1 && l2) {
-        if(l1.val <= l2.val) {
-            l3.next = l1;
-            l1 = l1.next;
-        } else {
-            l3.next = l2;
-            l2 = l2.next;
-        }
+//     if(!list1) {
+//         return list2;
+//     }
+    
+//     if(!list2) {
+//         return list1;
+//     }
+    
+//     let l3 = {};
+//     let l1 = list1;
+//     let l2 = list2;
+    
+//     while(l1 && l2) {
+//         if(l1.val <= l2.val) {
+//             l3.next = l1;
+//             l1 = l1.next;
+//         } else {
+//             l3.next = l2;
+//             l2 = l2.next;
+//         }
         
-        l3 = l3.next;
-    }
+//         l3 = l3.next;
+//     }
     
-    if(l1) {
-        l3.next = l1;
-    } else {
-        l3.next = l2;
-    }
+//     if(l1) {
+//         l3.next = l1;
+//     } else {
+//         l3.next = l2;
+//     }
     
-    if(list1.val <= list2.val) {
-        return list1;
-    } else {
-        return list2;
-    }
+//     if(list1.val <= list2.val) {
+//         return list1;
+//     } else {
+//         return list2;
+//     }
 }
