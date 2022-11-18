be9a57f561d3de425de07a623cced9295ff5e38b
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-18 09:48:42 -0800

    Time: 135 ms (10.11%), Space: 44.9 MB (80.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,19 +11,23 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return 0;
-    let max = 0;
+    if(!root) return 0;
+  let right = maxDepth(root.right);
+  let left = maxDepth(root.left);
+  return 1 + Math.max(right, left);
+//     if (!root) return 0;
+//     let max = 0;
     
-    const searchDepth = (node, depth) => {
-        max = Math.max(max, depth);
-        if (node.left) {
-            searchDepth(node.left, depth + 1);
-        }
+//     const searchDepth = (node, depth) => {
+//         max = Math.max(max, depth);
+//         if (node.left) {
+//             searchDepth(node.left, depth + 1);
+//         }
         
-        if (node.right) {
-            searchDepth(node.right, depth + 1);
-        }
-    }
-    searchDepth(root, 1)
-    return max;
+//         if (node.right) {
+//             searchDepth(node.right, depth + 1);
+//         }
+//     }
+//     searchDepth(root, 1)
+//     return max;
 };
