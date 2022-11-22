78dec27b6ed53663eef4ce22a8bab005d68d8a97
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-21 21:54:17 -0800

    Time: 245 ms (63.56%), Space: 46.3 MB (61.44%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -3,17 +3,29 @@
  * @return {number}
  */
 var numSquares = function(n) {
-    const memo = [0, 1, 2, 3, 1];
-    
-    const dfs = (num) => {
-        if (memo[num] !== undefined) return memo[num];
-        let min = Infinity;
-        for (let i = Math.floor(num ** 0.5); i >= 1; i--) {
-            min = Math.min(min, 1 + dfs(num - (i ** 2)));
-        }
-        memo[num] = min;
-        return min;
+    const dp = Array(n + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(
+        dp[i],
+        dp[i - j * j] + 1,
+      );
     }
+  }
+  return dp[n];
+//     const memo = [0, 1, 2, 3, 1];
     
-    return dfs(n);
+//     const dfs = (num) => {
+//         if (memo[num] !== undefined) return memo[num];
+//         let min = Infinity;
+//         for (let i = Math.floor(num ** 0.5); i >= 1; i--) {
+//             min = Math.min(min, 1 + dfs(num - (i ** 2)));
+//         }
+//         memo[num] = min;
+//         return min;
+//     }
+    
+//     return dfs(n);
 };
