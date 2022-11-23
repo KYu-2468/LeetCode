ae0948680b7abba79fd70c58994b8101209be8d0
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-22 17:15:23 -0800

    Time: 136 ms (43.12%), Space: 44.9 MB (69.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -3,27 +3,18 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const rowArr = new Array(9);
-    const colArr = new Array(9);
-    const secArr = new Array(9);
-    generateArray(rowArr);
-    generateArray(colArr);
-    generateArray(secArr);
+    const rowArr = generateArray(9);
+    const colArr = generateArray(9);
+    const secArr = generateArray(9);
     
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
             if (board[row][col] === ".") continue;
-            if (rowArr[row].has(board[row][col])) {
-                return false;
-            }
+            if (rowArr[row].has(board[row][col])) return false;
             rowArr[row].add(board[row][col]);
-            if (colArr[col].has(board[row][col])) {
-                return false;
-            }
+            if (colArr[col].has(board[row][col])) return false;
             colArr[col].add(board[row][col]);
-            if (secArr[getSec(row, col)].has(board[row][col])) {
-                return false;
-            }
+            if (secArr[getSec(row, col)].has(board[row][col])) return false;
             secArr[getSec(row, col)].add(board[row][col]);
         }
     }
@@ -32,12 +23,12 @@ var isValidSudoku = function(board) {
 };
 
 function getSec(row, col) {
-    const sec = 3 * Math.floor(row / 3) + Math.floor(col / 3);
-    return sec;
+    return 3 * Math.floor(row / 3) + Math.floor(col / 3);
 }
 
-function generateArray(arr) {
-    for (let i = 0; i< arr.length; i++) {
+function generateArray(length) {
+    const arr = new Array(length);
+    for (let i = 0; i < arr.length; i++) {
         arr[i] = new Set();
     }
     return arr;
