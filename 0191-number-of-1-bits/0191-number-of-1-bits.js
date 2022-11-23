4af8385873078f0ad3f0740a35839a7ce73cc5e3
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-23 08:58:57 -0800

    Time: 96 ms (70.39%), Space: 42.5 MB (58.48%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,23 +3,15 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    const str = n.toString(2); // '110'
-  let counter = 0;
-  
-  for (let i = str.length-1; i >= 0; i--) {
-    if (str[i] == '1') counter++;
-  }
-  
-  return counter;
-//     let count = 0;
+    let count = 0;
     
-//     while(n) {
-//         if(n & 1 === 1) {
-//             count++;
-//         }
+    while(n) {
+        if(n & 1 === 1) {
+            count++;
+        }
         
-//         n = n >>> 1
-//     }
+        n = n >>> 1
+    }
     
-//     return count;
+    return count;
 };
