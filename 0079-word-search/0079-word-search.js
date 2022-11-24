cff6c1aa801a34e577c11d6a0f985c8085b6cba4
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-23 16:17:02 -0800

    Time: 903 ms (56.61%), Space: 44.4 MB (61.33%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -3,16 +3,43 @@
  * @param {string} word
  * @return {boolean}
  */
+const dirs = [ [-1, 0 ], [1, 0], [0, -1], [0, 1] ];
 var exist = function(board, word) {
     
-    for(let r = 0; r < board.length; r++) {
-        for(let c = 0; c < board[r].length; c++) {
-            if(board[r][c] === word[0] && dfs(board, word, r, c)) return true;
+    if(board == null || word == null) return false;
+    const chars = word.split("");
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[i].length; j++){
+            if(match(board, i, j, chars, 0)){
+                return true;
+            }
         }
     }
-    
     return false;
-};
+}
+
+function match(board, i, j, chars, index){
+    if (index == chars.length) return true;
+    if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != chars[index]) return false;
+    board[i][j] = 0;
+    let result = false;
+    for (const dir of dirs) {
+        result |= match(board, i + dir[0], j + dir[1], chars, index + 1);
+        if (result) break;
+    }
+    board[i][j] = chars[index];
+    return result;
+}
+    
+    
+//     for(let r = 0; r < board.length; r++) {
+//         for(let c = 0; c < board[r].length; c++) {
+//             if(board[r][c] === word[0] && dfs(board, word, r, c)) return true;
+//         }
+//     }
+    
+//     return false;
+// };
 
 const dfs = (board, word, row, col, visited = {}) => {
     // console.log(row, col, word, visited)
