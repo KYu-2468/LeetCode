3860625471ac7b3be37218c74a01e648bae90058
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-26 16:37:45 -0800

    Time: 114 ms (96.92%), Space: 62 MB (27.69%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
@@ -12,6 +12,7 @@ var jobScheduling = function(startTime, endTime, profit) {
     
     jobs.sort((a, b) => a[1] - b[1] || a[0] - b[0]);
     const dp = [jobs[0][2]];
+    
     for (let i = 1; i < jobs.length; i++) {
         const [s, e, p] = jobs[i];
         
@@ -22,20 +23,28 @@ var jobScheduling = function(startTime, endTime, profit) {
             dp.push(Math.max(p, dp[dp.length - 1]));
             continue;
         }
-        let left = 0, right = i - 1;
-        while (left < right) {
-            const mid = Math.floor((left + right) / 2);
-            if (s < jobs[mid][1]) {
-                right = mid - 1;
-            } else if (left === mid){
-                break;
-            } else {
-                left = mid;
-            }
-        }
-        if (s >= jobs[right][1]) left = right;
-        dp.push(Math.max(dp[dp.length - 1], dp[left] + p));
+        const index = binarySearch(jobs, i);
+        dp.push(Math.max(dp[dp.length - 1], dp[index] + p));
     }
-    // console.log(dp)
+    
     return dp[dp.length - 1];
 };
+
+function binarySearch(jobs, i) {
+    const s = jobs[i][0]
+    let left = 0, right = i - 1;
+    
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (s < jobs[mid][1]) {
+            right = mid - 1;
+        } else if (left === mid){
+            break;
+        } else {
+            left = mid;
+        }
+    }
+    
+    if (s >= jobs[right][1]) left = right;
+    return left;
+}
