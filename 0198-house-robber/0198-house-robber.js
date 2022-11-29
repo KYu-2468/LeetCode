d4ff7f0112e159d84273a5599a92e2c99570e947
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-11-29 01:34:27 -0800

    Time: 116 ms (7.79%), Space: 41.6 MB (85.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,17 +3,24 @@
  * @return {number}
  */
 var rob = function(nums) {
-    
     if(nums.length === 1) {
         return nums[0]
     }
     
-    let prev1 = Math.max(nums[1], nums[0]);
-    let prev2 = nums[0];
-        
-    for(let i = 2; i < nums.length; i++) {
-        [prev2, prev1] = [prev1, Math.max(prev1, nums[i] + prev2)];
+    const memo = [nums[0]];
+    
+    for (let i = 1; i < nums.length; i++) {
+        memo[i] = Math.max(memo[i - 1], (memo[i - 2] || 0) + nums[i]);
     }
     
-    return prev1;
+    return memo[memo.length - 1];
+    
+//     let prev1 = Math.max(nums[1], nums[0]);
+//     let prev2 = nums[0];
+        
+//     for(let i = 2; i < nums.length; i++) {
+//         [prev2, prev1] = [prev1, Math.max(prev1, nums[i] + prev2)];
+//     }
+    
+//     return prev1;
 };
