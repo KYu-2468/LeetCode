e70228fbffb5eaf86380c9632ce9ebab4e0413c3
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-12-02 04:44:18 -0800

    Time: 86 ms (69.34%), Space: 42.2 MB (37.87%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,26 +4,25 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if(!needle) {
-      return 0;
-    }
-    let haystackArr = haystack.split("");
-    const first = needle[0];
-    let buffer = 0;
+    if(!needle) return 0;
+    if(!haystack) return -1;
+    if(haystack.length<needle.length) return -1;
 
-    while(haystackArr.indexOf(first) !== -1) {
-      if (haystackArr.length < needle.length) {
-        break;
-      }
-      const index = haystackArr.indexOf(first);
-      const str = haystack.substring(index, index + needle.length);
-      if (str === needle) {
-        return index + buffer;
-      } else {
-        haystackArr = haystackArr.splice(index + 1);
-        haystack = haystack.substring(index + 1);
-        buffer += index + 1;
-      }
+    
+    let i=0;
+    let j=0;
+    
+    while(i<haystack.length && j<needle.length){
+        
+        if (haystack[i]===needle[j]){
+            ++i;
+            ++j;
+        } else {
+            i = (++i) - j;
+            j = 0;
+        }
     }
+    if(j===needle.length) return i-j;
+
     return -1;
 };
