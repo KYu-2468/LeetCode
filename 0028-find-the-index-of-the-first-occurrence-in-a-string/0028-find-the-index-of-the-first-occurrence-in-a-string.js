59ed1d9a28edb28d8cf26612bd8f196e4f7b204a
remaerd <101434685+KYu-2468@users.noreply.github.com>
2022-12-04 21:48:18 -0800

    Time: 67 ms (89.03%), Space: 42 MB (64.33%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,13 +4,5 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    // "bloombergmb" "mbe"
-    if (haystack.length < needle.length) return -1;
     return haystack.indexOf(needle);
-//     for (let i = 0; i < haystack.length; i++) {
-//         const substr = haystack.slice(i, i + needle.length);
-//         if (substr === needle) return i;
-//     }
-    
-//     return -1;
 };
